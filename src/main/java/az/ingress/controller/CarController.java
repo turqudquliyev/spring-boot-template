5a10acb0d4926e4680ed726298cf4d5a5524e25f
rashadamanovv <rashadamanovv@gmail.com>
2024-07-15 04:35:10 -0700

    simple rest api with profiling

--- src/main/java/az/ingress/controller/CarController.java
+++ src/main/java/az/ingress/controller/CarController.java
@@ -2,12 +2,12 @@ package az.ingress.controller;
 
 import az.ingress.service.CarService;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.http.ResponseEntity.ok;
 @RequestMapping("v1/services")
 @RestController
 public class CarController {
@@ -19,6 +19,7 @@ public class CarController {
     }
 
     @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
     public String runService(){
         return carService.run();
     }
