731cf91da435dcb6529269cceef0e96fc9c6d98e
turqudquliyev <turqudquliyev@gmail.com>
2024-09-21 13:42:06 +0400

    feat: HealthControllerTest has been added

---
+++ src/main/java/az/ingress/exception/ErrorHandler.java
@@ -0,0 +1,30 @@
+package az.ingress.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static az.ingress.exception.ErrorMessage.UNEXPECTED_ERROR;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.METHOD_NOT_ALLOWED;
+
+@Slf4j
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    public ErrorResponse handle(Exception ex) {
+        log.error("Exception: ", ex);
+        return new ErrorResponse(UNEXPECTED_ERROR.getMessage());
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    @ResponseStatus(METHOD_NOT_ALLOWED)
+    public ErrorResponse handle(HttpRequestMethodNotSupportedException ex) {
+        log.error("HttpRequestMethodNotSupportedException: ", ex);
+        return new ErrorResponse(ex.getMessage());
+    }
+}
